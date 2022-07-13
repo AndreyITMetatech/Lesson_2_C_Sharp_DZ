75ab61520a9bab735c1146cc1f39846c2fee3f6c
AndreyITMetatech <rfandrey@yandex.ru>
2022-07-13 13:52:46 +0300

    Добавил решение задач из черновика

--- task_10/Program.cs
+++ task_10/Program.cs
@@ -3,3 +3,8 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.Write("Введите трёхзначное число ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Вторая цифра этого числа -> " + stringNumber[1]);
--- task_15/Program.cs
+++ task_15/Program.cs
@@ -3,3 +3,21 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("(Этот день выходной) -> да");
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("Это не является выходным");
+    }
+    else Console.WriteLine("(Этот день не является выходным) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
